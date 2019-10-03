278a18b97080f45e0f67155caae5cac802bf0c1f
Dany Fariña <danyluis@gmail.com>
2019-10-03 16:52:31 +0200

    Update 2019-10-03

--- modules/utility/init.zsh
+++ modules/utility/init.zsh
@@ -114,10 +114,10 @@ else
 fi
 
 alias l='ls -1A'         # Lists in one column, hidden files.
-alias ll='ls -lh'        # Lists human readable sizes.
-alias lr='ll -R'         # Lists human readable sizes, recursively.
-alias la='ll -A'         # Lists human readable sizes, hidden files.
-alias lm='la | "$PAGER"' # Lists human readable sizes, hidden files through pager.
+alias ll='ls -lhAF'        # Lists human readable sizes.
+alias lr='ll -RF'         # Lists human readable sizes, recursively.
+alias la='ll -AF'         # Lists human readable sizes, hidden files.
+alias lm='laF | "$PAGER"' # Lists human readable sizes, hidden files through pager.
 alias lx='ll -XB'        # Lists sorted by extension (GNU only).
 alias lk='ll -Sr'        # Lists sorted by size, largest last.
 alias lt='ll -tr'        # Lists sorted by date, most recent last.
